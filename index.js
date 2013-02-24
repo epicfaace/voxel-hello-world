f13c04a669f17df7414e04e8b22816126e143083
epicfaace <aramaswamis@gmail.com>
2013-02-23 21:48:40 -0500

    new experimentation
    added comments, made the old index.js into index-old.js

--- index.js
+++ index.js
@@ -1,70 +1,29 @@
-var createGame = require('voxel-engine')
-var voxel = require('voxel')
-var toolbar = require('toolbar')
-var player = require('voxel-player')
-var createTerrain = require('voxel-perlin-terrain')
-var highlighter = require('voxel-highlight')
-var createTree = require('voxel-forest')
-var texturePath = require('painterly-textures')(__dirname)
-var blockSelector = toolbar({el: '#tools'})
-
-// setup the game and add some trees
+var createGame = require('voxel-engine');
+var player = require('voxel-player');
+var texturePath = require('painterly-textures')(__dirname);
+var createTerrain = require('voxel-perlin-terrain');
 var game = createGame({
   generateVoxelChunk: createTerrain({ scaleFactor: 10 }),
-  chunkDistance: 2,
+  chunkDistance: 2, //2 in front, 2 behind, etc. for 16 chunks total
   materials: [
     'obsidian',
-    ['grass', 'dirt', 'grass_dirt'],
+    ['grass', 'dirt', 'grass_dirt'], //multiple sides
     'brick',
     'grass',
     'plank'
+    //materials [1,2,3...etc]; index[0] is air, NOT the first item.
   ],
   texturePath: texturePath,
   worldOrigin: [0, 0, 0],
   controls: { discreteFire: true }
-})
-
-window.game = game // for debugging
-
-var container = document.querySelector('#container')
-
-game.appendTo(container)
-
-for (var i = 0; i < 20; i++) createTree(game, { bark: 5, leaves: 4 })
+});
 
 // create the player from a minecraft skin file and tell the
 // game to use it as the main player
-var createPlayer = player(game)
-var substack = createPlayer('substack.png')
-substack.yaw.position.set(0, -1200, 0)
-substack.possess()
+var createPlayer = player(game);
+var substack = createPlayer('substack.png');
+substack.yaw.position.set(0, -1200, 0);
+substack.possess();
 
-// toggle between first and third person modes
-window.addEventListener('keydown', function (ev) {
-  if (ev.keyCode === 'R'.charCodeAt(0)) substack.toggle()
-})
-
-// block interaction stuff
-var highlight = highlighter(game)
-var currentMaterial = 1
-
-blockSelector.on('select', function(material) {
-  material = +material // cast to int
-  if (material > -1) currentMaterial = material
-  else currentMaterial = 1
-})
-
-game.on('fire', function(target, state) {
-  var vec = game.cameraVector()
-  var pos = game.cameraPosition()
-  var point = game.raycast(pos, vec, 100)
-  if (!point) return
-  var erase = !state.firealt && !state.alt
-  if (erase) {
-    game.setBlock(point, 0)
-  } else {
-    game.createBlock(point.addSelf(vec.multiplyScalar(-game.cubeSize/2)), currentMaterial)
-  }
-})
-
-module.exports = game
+var container = document.querySelector('#container')
+game.appendTo(container);
